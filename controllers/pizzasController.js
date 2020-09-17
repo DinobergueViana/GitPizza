292d246b202acfef67e2e1d337a876d6fe047652
DinobergueViana <dinobergueviana@gmail.com>
2020-09-17 18:12:26 -0300

    adicionando arquivos

---
+++ controllers/pizzasController.js
@@ -0,0 +1,47 @@
+const listaPizzas = require('../data-base/pizzas.json');
+const fs = require('fs');
+const path = require('path');
+
+const pizzasController = {
+    index: (req, res) => {
+        res.render('index', {listaPizzas});
+    },
+    show: (req, res) => {
+        let indice = req.params.indice;
+        let pizza = listaPizzas[indice];
+        res.render('pizza',{pizza});
+
+    },
+    adicionar: (req, res) => {
+        res.render('addPizza');
+    },
+    editar: (req, res) => {
+        let pizza = listaPizzas[0]
+        res.render('editarPizza', {pizza});
+    },
+    atualizarPizza: (req, res) => {
+
+    },
+    salvarPizza: (req, res) => {
+        let {nome, preco, ingredientes} = req.body;
+        // transforma string em array
+        ingredientes = ingredientes.split(',');
+        // cria o id do registro
+        let id = listaPizzas.length + 1
+
+        //adiciona um registro de pizza na base de dados
+        listaPizzas.push({
+            id: id,
+            nome: nome,
+            preco: preco,
+            img: "",
+            ingredientes: ingredientes
+        });
+
+        //ecreve o arquivo na base de dados
+        fs.writeFileSync(path.join('data-base', 'pizzas.json'), JSON.stringify(listaPizzas));
+        res.send(listaPizzas);
+    }
+}
+
+module.exports = pizzasController;
---
+++ routes/PizzaRouter.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const pizzasController = require('../controllers/pizzasController');
+// const itemController = require('../controllers/itemController');
+// outra opção de require
+// const { Router } = require('express')
+const router = express.Router();
+
+router.get('/', pizzasController.index);
+
+router.get('/pizza/:indice', pizzasController.show);
+router.get('/add', pizzasController.adicionar);
+router.post('/add', pizzasController.salvarPizza);
+router.get('/editar', pizzasController.editar);
+router.post('editar', pizzasController.atualizarPizza);
+
+
+module.exports = router;
+
